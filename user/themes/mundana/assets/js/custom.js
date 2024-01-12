01758931c38c6ec6718f88b193de6c614a4db13e
IceCreamWW <wangwei.sjtu@sjtu.edu.cn>
2024-01-12 13:23:48 +0800

    todo: fix navbar menu on mobile devices

--- user/themes/mundana/assets/js/custom.js
+++ user/themes/mundana/assets/js/custom.js
@@ -1,1 +1,25 @@
 /* Custom js code for theme */
+
+
+$(document).ready(function() {
+    function adjustBannerHeight() {
+        // if match media lg
+        if (window.matchMedia('(min-width: 992px)').matches)
+            var ref = $('#intro'); // Replace with your image element's ID
+        else
+            var ref = $('#carouselIntroIndicators'); // Replace with your image element's ID
+        var banner = $('#homeBanner'); // Replace with your background element's ID
+
+        if (ref.length && banner.length) {
+            var refBottom = ref.offset().top + ref.outerHeight();
+            banner.height(refBottom);
+        }
+    }
+
+    // Adjust the height initially
+    adjustBannerHeight();
+
+    // Adjust the height on window resize
+    $(window).resize(adjustBannerHeight);
+});
+
